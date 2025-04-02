ea8447f869382b5d774ad70797da58f65f6ed886
nainarwt1 <nainamailbox10@gmail.com>
2025-04-02 11:31:37 +0530

    gvn

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,24 @@
+//function sayHello(){
+// let you=prompt("what is your name");
+//console.log("hello",you+"I");
+//}
+//sayHello();
+
+/* let varContainingFunction = function(){
+let varInFunction= "i m in a funcion ";
+console.log("hi", varInFunction);
+};
+varContainingFunction(); */
+
+/* function tester(para1,para2){
+    return para1+ " "+para2;
+}
+const arg1="argument 1";
+const arg2="argument 2";
+console.log(tester(args1,args2)) */
+function add(x,y){
+    let x=prompt("enter a number");
+    let y=prompt("enter a number");
+    console.log(x+y);
+}
+add()
