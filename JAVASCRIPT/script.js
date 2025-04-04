43d323eda9365ff946ec4c7bae9d8d73fed62db3
nainarwt1 <nainamailbox10@gmail.com>
2025-04-04 12:32:49 +0530

    hhg

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -16,9 +16,33 @@ varContainingFunction(); */
 const arg1="argument 1";
 const arg2="argument 2";
 console.log(tester(args1,args2)) */
-function add(x,y){
+/* function add(x,y){
     let x=prompt("enter a number");
     let y=prompt("enter a number");
     console.log(x+y);
+    add()
+} */
+/* function addTwoNumbers(x,y) {
+    console.log(x+y);
+    addTwoNumbers();
+    addTwoNumbers(6,6);
+    addTwoNumbers(10);
+}   */
+/* function stuff(x){
+    console.log(x);
+let ArrowStuff =x;
+ console.log (x);
+} */
+/* let addTwoNumbers=(x,y)
+console.log(x+y);
+addTwoNumbers(5,3); */
+/* let sayHi = ()=>
+console.log("hi"); */
+/* let spread = ["so","much","fun"];
+let message=["js","is",...spread,"and","very","powerful"]; */
+function addTwoNumbers(x,y) {
+    console.log(x+y);
 }
-add()
+let arr= [5,9];
+addTwoNumbers(...arr);
+
