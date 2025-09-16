f14489b0c210b017e783cfcaed08635ff127fb64
nainarwt1 <nainamailbox10@gmail.com>
2025-09-16 15:25:49 +0530

    bjll

---
+++ HTML/function.js
@@ -0,0 +1,48 @@
+//function sayHello(){
+// let you=prompt("what is your name");
+//console.log("hello",you+"I");
+//}
+//sayHello();
+
+/* let varContainingFunction = function(){
+let varInFunction= "i m in a funcion ";
+console.log("hi", varInFunction);
+};
+varContainingFunction(); */
+
+/* function tester(para1,para2){
+    return para1+ " "+para2;
+}
+const arg1="argument 1";
+const arg2="argument 2";
+console.log(tester(args1,args2)) */
+/* function add(x,y){
+    let x=prompt("enter a number");
+    let y=prompt("enter a number");
+    console.log(x+y);
+    add()
+} */
+/* function addTwoNumbers(x,y) {
+    console.log(x+y);
+    addTwoNumbers();
+    addTwoNumbers(6,6);
+    addTwoNumbers(10);
+}   */
+/* function stuff(x){
+    console.log(x);
+let ArrowStuff =x;
+ console.log (x);
+} */
+/* let addTwoNumbers=(x,y)
+console.log(x+y);
+addTwoNumbers(5,3); */
+/* let sayHi = ()=>
+console.log("hi"); */
+/* let spread = ["so","much","fun"];
+let message=["js","is",...spread,"and","very","powerful"]; */
+function addTwoNumbers(x,y) {
+    console.log(x+y);
+}
+let arr= [5,9];
+addTwoNumbers(...arr);
+
